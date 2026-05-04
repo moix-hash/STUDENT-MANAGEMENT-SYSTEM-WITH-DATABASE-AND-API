a7c7318bcaef523f15fa72482a37e8887054646d
SYED MOIZ SHAHAB <moizshahab262@gmail.com>
2026-05-04 13:31:34 +0500

    Update database.py

--- STUDENT MANAGEMENT SYSTEM WITH DATABASE AND API/database.py
+++ STUDENT MANAGEMENT SYSTEM WITH DATABASE AND API/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLite Database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./students.db"
 
 engine = create_engine(
@@ -12,7 +11,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency to provide a database session to endpoints
 def get_db():
     db = SessionLocal()
     try:
