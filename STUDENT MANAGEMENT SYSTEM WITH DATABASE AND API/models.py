9922134b48c20eaf90777a1c3229b0b58c91ddda
SYED MOIZ SHAHAB <moizshahab262@gmail.com>
2026-05-04 13:31:17 +0500

    Update models.py

--- STUDENT MANAGEMENT SYSTEM WITH DATABASE AND API/models.py
+++ STUDENT MANAGEMENT SYSTEM WITH DATABASE AND API/models.py
@@ -4,8 +4,8 @@ from database import Base
 class Student(Base):
     __tablename__ = "students"
 
-    id = Column(Integer, primary_key=True, index=True)  # Primary Key
-    name = Column(String)                               # Student Name
-    email = Column(String, unique=True, index=True)     # Unique Email
-    age = Column(Integer)                               # Student Age
-    course = Column(String)                             # Enrolled Course
+    id = Column(Integer, primary_key=True, index=True)  
+    name = Column(String)                              
+    email = Column(String, unique=True, index=True)     
+    age = Column(Integer)                               
+    course = Column(String)                            
